9a21d7b72b3b63d7dd0b04ca5b743b486716e0a6
Amanthink <colamanit@gmail.com>
2026-07-01 20:34:20 +0530

    div 3 2 solution + 1 daily 1200 rated

---
+++ D_Three_Activities.cpp
@@ -0,0 +1,107 @@
+/**
+ *    author:  colaman
+ *    github:  Amanthink
+ *    created: 30.06.2026 14:35:07
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+// ================= TYPES (long name -> short name) =================
+typedef long long ll;                 // long long -> ll
+typedef unsigned long long ull;       // unsigned long long -> ull
+typedef long double ld;               // long double -> ld
+
+typedef pair<int,int> pii;            // pair<int,int> -> pii
+typedef pair<ll,ll> pll;              // pair<long long,long long> -> pll
+
+typedef vector<int> vi;               // vector<int> -> vi
+typedef vector<ll> vll;               // vector<long long> -> vll
+typedef vector<pii> vpii;             // vector<pair<int,int>> -> vpii
+typedef vector<vector<int>> vvi;      // vector<vector<int>> (2D grid) -> vvi
+typedef vector<string> vs;            // vector<string> -> vs
+
+typedef map<int,int> mii;             // map<int,int> -> mii
+typedef set<int> si;                  // set<int> -> si
+typedef unordered_map<int,int> umii;  // unordered_map<int,int> -> umii
+
+// ================= FUNCTION/METHOD SHORTCUTS =================
+#define pb push_back               // v.push_back(x) -> v.pb(x)
+#define eb emplace_back            // v.emplace_back(x) -> v.eb(x)
+#define po pop_back                // v.pop_back() -> v.po()
+#define mp make_pair                // make_pair(a,b) -> mp(a,b)
+#define ff first                    // p.first -> p.ff
+#define ss second                   // p.second -> p.ss
+
+#define all(x) (x).begin(), (x).end()   // v.begin(),v.end() -> all(v)
+#define sz(x) (int)(x).size()           // (int)v.size() -> sz(v)
+#define rsz(x,n) (x).resize(n)          // v.resize(n) -> rsz(v,n)
+
+// ================= LOOP SHORTCUTS =================
+/*#define for(i,a,b) for (int i = (a); i < (b); i++)   // for(i=a;i<b;i++) -> FOR(i,a,b)
+#define rep(i,n) for(i,0,n)                          // for(i=0;i<n;i++) -> REP(i,n)
+#define rof(i,n) for (int i = (n)-1; i >= 0; i--)    // for(i=n-1;i>=0;i--) -> ROF(i,n)
+*/
+// ================= USEFUL CONSTANTS =================
+const ll inf = 1e18;        // a "big enough" number, used as infinity
+const int mod = 1e9 + 7;    // common modulo used in many problems
+const ld pi= acos(-1.0);   // value of pi
+
+// ================= FAST IO =================
+#define endl "\n"           // endl flushes (slow); "\n" doesn't (fast)
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n), c(n);
+
+    for (int &x : a) cin >> x;
+    for (int &x : b) cin >> x;
+    for (int &x : c) cin >> x;
+
+    vector<pii> A, B, C;
+
+    for (int i = 0; i < n; i++) {
+        A.push_back({a[i], i});
+        B.push_back({b[i], i});
+        C.push_back({c[i], i});
+    }
+
+    sort(A.rbegin(), A.rend());
+    sort(B.rbegin(), B.rend());
+    sort(C.rbegin(), C.rend());
+
+    long long ans = 0;
+
+    for (int i = 0; i < min(3, n); i++) {
+        for (int j = 0; j < min(3, n); j++) {
+            for (int k = 0; k < min(3, n); k++) {
+
+                int ia = A[i].second;
+                int ib = B[j].second;
+                int ic = C[k].second;
+
+                if (ia != ib && ib != ic && ia != ic) {
+                    ans = max(ans,
+                              1LL * A[i].first +
+                              B[j].first +
+                              C[k].first);
+                }
+            }
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+}
